64a554c6a94c62371ed7cd89fb7592b2d7ef9f8f
apple <bronzels@hotmail.com>
2025-01-08 22:26:26 +0800

    1, hands-on-rl.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-git clone git@github.com:boyu-ai/Hands-on-RL.git
-git clone git@github.com:datawhalechina/easy-rl.git
-git clone git@github.com:datawhalechina/joyrl-book.git
+git clone git@github.com:boyu-ai/Hands-on-RL.git Hands-on-RL-orig
+pip install numpy matplotlib
+
+git clone git@github.com:datawhalechina/easy-rl.git easy-rl-orig
+git clone git@github.com:datawhalechina/joyrl-book.git joyrl-book-orig
+
